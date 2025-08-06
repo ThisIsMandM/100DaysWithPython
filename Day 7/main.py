79d565a2324761bfb5b9bfa3f91877c6bffdf77e
MandM <mhmdrzaoncode@gmail.com>
2025-08-06 20:36:00 +0330

    step 4, and its over, got em! _/

--- Day 7/main.py
+++ Day 7/main.py
@@ -1,8 +1,66 @@
 import random
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+final_word = ""
 lives = 6
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
-print(chosen_word)
+
 place= ""
 for placeholder in range(len(chosen_word)):
     place += "_"
@@ -12,24 +70,26 @@ correct_guesses= []
 
 while not game_over:
     guess = input("Guess a letter: ").lower()
-    
     display = ""
+   
     
     for letter in chosen_word:
         if letter == guess:
-            correct_guesses.append(guess)
             display += letter
+            correct_guesses.append(guess)
         elif letter in correct_guesses:
             display += letter
         else:
             display += "_"
 
-    if guess not in chosen_word:
-        lives -= 1
+    print(display)
 
+    if guess not in chosen_word:
+        lives -=1
+        if lives == 0:
+            game_over = True
+            print("You lose!")
     if "_" not in display:
         game_over = True
-        print("You win.")
-
-final_word = str(correct_guesses)
-print(final_word)
+        print(f"The word is {display},You win.")
+    print(stages[lives])
