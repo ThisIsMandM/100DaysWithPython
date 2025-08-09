f94e369c551cf4d6a8342fb1c9afbdf3be47b0df
MandM <mhmdrzaoncode@gmail.com>
2025-08-09 17:15:17 +0330

    New day, here is function, parameter, argument

---
+++ Day 8/main.py
@@ -0,0 +1,15 @@
+# def greet(name):
+#     print("Hi " + name)
+#     print("Go")
+#     print("Away")
+
+
+# greet("George")
+
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    input(f"What is it like in {location}?\n")
+
+greet_with("Noah River", "Neverland")
+
