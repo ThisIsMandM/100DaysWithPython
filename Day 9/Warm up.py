ab903a3792d2e54a2000f7c382f5854ed0aa0591
MandM <mhmdrzaoncode@gmail.com>
2025-08-10 21:15:08 +0330

    Second part of warm up

--- Day 9/Warm up.py
+++ Day 9/Warm up.py
@@ -1,4 +1,4 @@
-programming_dictionary = {
+""" programming_dictionary = {
     "Bug": "An error in a program that prevent the program from running as expected.",
     "Function": "A piece of code that you can easily call over and over again.",
 }
@@ -12,3 +12,30 @@ programming_dictionary["Argument"] = "It's the actual value of the parameter."
 for thing in programming_dictionary:
     print(thing)
     print(programming_dictionary[thing])
+     """
+
+capitals = {
+    "USA": "Washington",
+    "Germany": "Berlin"
+}
+
+#Nested List in Dictionary
+
+travel_log = {
+    "USA": ["Washington", "New york", "Nashville"],
+    "Germany": ["Berlin", "Dortmund"],
+    "Italy": {
+        "cities_visited": ["Rome", "Milan", "Turin"],
+        "total_visits": 5
+    }
+}
+
+print(travel_log["Italy"]["cities_visited"])
+print(travel_log["USA"][2])
+
+nested_list = ["A", "B", ["C", "D"]]
+
+print(nested_list[2][1])
+
+print(travel_log["Germany"][0])
+
