3c476f255654f27372224b667514fe70fcbe0f83
MandM <mhmdrzaoncode@gmail.com>
2025-08-17 13:09:28 +0330

    The Calculator Project

--- Day 10/The Calculator Project.py
+++ Day 10/The Calculator Project.py
@@ -11,7 +11,7 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return n1 / n2
 
-first_number = int(input("What is your first number? "))
+first_number = float(input("What is your first number? "))
 operations = {
     "+" : add,
     "-" : minus,
@@ -20,11 +20,12 @@ operations = {
 }
 calculater_on = True
 while calculater_on:
-    print(operations[str(input("+\n-\n*\n/\n Which operation do you want to perform: "))](first_number, int(input("What is your second number? "))))
+    print(operations[str(input("+\n-\n*\n/\n Which operation do you want to perform: "))](first_number, float(input("What is your second number? "))))
     while True:
         cal_again = str(input("Do you wish to use calculator now, again? 'y for yes and n for no' ")).lower()
         if cal_again == "n":
             calculater_on = False
+            print("See You Yext Time!")
             break
         if cal_again == "y":
             calculater_on = True
