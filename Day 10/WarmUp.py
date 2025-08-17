25d2d54ed69aa8d5eafe567197697f6b77c25fb9
MandM <mhmdrzaoncode@gmail.com>
2025-08-17 12:39:38 +0330

    Day 10 part 2 of warm up and setup for the main project of the day

--- Day 10/WarmUp.py
+++ Day 10/WarmUp.py
@@ -1,4 +1,4 @@
-def format_name(f_name, l_name):
+"""def format_name(f_name, l_name):
     formated_f_name = str(f_name).title()
     formated_l_name = str(l_name).title()
     return f"{formated_l_name} {formated_f_name}"
@@ -17,3 +17,18 @@ output = function1("history")
 output2 = function2(output)
 
 print(output2)
+
+"""
+# Part 2
+# Docstring:
+
+def format_name(f_name, l_name):
+    """Take a first and last name format it to return the title case version of the name."""
+    if f_name == "" or l_name == "":
+        return
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"{formated_f_name} {formated_l_name}"
+
+
+print(format_name(input("What is your first name?"), input("What is your last name")))
