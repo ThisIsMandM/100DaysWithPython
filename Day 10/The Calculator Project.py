7446a1768cccfbb6b274327959fe830da719d5d6
MandM <mhmdrzaoncode@gmail.com>
2025-08-17 13:15:50 +0330

    The Calculator Project, zero bug version!

--- Day 10/The Calculator Project.py
+++ Day 10/The Calculator Project.py
@@ -20,15 +20,19 @@ operations = {
 }
 calculater_on = True
 while calculater_on:
-    print(operations[str(input("+\n-\n*\n/\n Which operation do you want to perform: "))](first_number, float(input("What is your second number? "))))
-    while True:
-        cal_again = str(input("Do you wish to use calculator now, again? 'y for yes and n for no' ")).lower()
-        if cal_again == "n":
-            calculater_on = False
-            print("See You Yext Time!")
-            break
-        if cal_again == "y":
-            calculater_on = True
-            break
-        else:
-            print("Invalid input, try again. ")
+    operation_choice = str(input("+\n-\n*\n/\n Which operation do you want to perform: "))
+    if operation_choice in operations:
+        print(operations[operation_choice](first_number, float(input("What is your second number? "))))
+        while True:
+            cal_again = str(input("Do you wish to use calculator now, again? 'y for yes and n for no' ")).lower()
+            if cal_again == "n":
+                calculater_on = False
+                print("See You Yext Time!")
+                break
+            if cal_again == "y":
+                calculater_on = True
+                break
+            else:
+                print("Invalid input, try again. ")
+    else:
+        calculater_on = True
