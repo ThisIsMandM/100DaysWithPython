b619ac3310bc77c9df029bf9db16b3f495bd6e68
MandM <mhmdrzaoncode@gmail.com>
2025-08-21 21:06:41 +0330

    Fixing the bug session part 3

---
+++ Day 13/03 Play Computer.py
@@ -0,0 +1,10 @@
+year = int(input("What's your year of birth?"))
+
+#if year > 1980 and year < 1994:
+if year > 1980 and year < 1995:
+    print("You are a millennial.")
+elif year > 1994:
+    print("You are a Gen Z.")
+
+
+# Problem: It always check higher or lower of 1994 and not the 1994 itself
