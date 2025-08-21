afae94aaab18df456f726fe5beaf8e4e0020cfc1
MandM <mhmdrzaoncode@gmail.com>
2025-08-21 21:21:16 +0330

    Fixing the bug session part 5

---
+++ Day 13/05 Squash bugs with a print statement.py
@@ -0,0 +1,8 @@
+# word_per_page = 0
+pages = int(input("Number of pages: "))
+# word_per_page == int(input("Number of words per page: ")) Problem: == means check if equal
+word_per_page = int(input("Number of words per page: "))
+total_words = pages * word_per_page
+print(total_words)
+# print(word_per_page) Problem: it prints 0
+
